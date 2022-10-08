1af722f1d74c30859c6d0f222ebcff9286e2716d
ndo22 <ocernosek@gmail.com>
2022-10-08 14:39:32 +0200

    Added Splash screen image and icons

--- app/src/main/java/com/example/chucknorrisapicompose/AnimatedSplashScreen.kt
+++ app/src/main/java/com/example/chucknorrisapicompose/AnimatedSplashScreen.kt
@@ -3,6 +3,7 @@ package com.example.chucknorrisapicompose
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
@@ -16,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -46,18 +48,21 @@ fun AnimatedSplashScreen(navController: NavHostController) {
 fun Splash(alpha: Float) {
     Box(
         modifier = Modifier
-            .background(if (isSystemInDarkTheme()) Color.Black else Purple700)
+            .background(if (isSystemInDarkTheme()) Color.Black else Color.White)
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Icon(
+       /* Icon(
             modifier = Modifier
                 .size(120.dp)
                 .alpha(alpha = alpha),
             imageVector = Icons.Default.Email,
             contentDescription = "Logo Icon",
             tint = Color.White
-        )
+        )*/
+
+        Image(painter = painterResource(id = R.drawable.chuck), contentDescription = "Image of chuck")
+
     }
 }
 
